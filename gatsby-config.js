3843cd1a4631ec97f4a6d3af94b70865103a1ee0
rainy-me <github@rainy.me>
2019-04-15 01:29:22 +0900

    site-source update 2019-04-15 01:29:22

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `浅色圆锥曲线爱好者`,
     description: `random notes`,
     author: `yue`,
-    theme_color: "#333333"
+    theme_color: "#333333",
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -13,26 +13,25 @@ module.exports = {
       options: {
         name: `src`,
         path: `${__dirname}/`,
-        ignore: [
-          `**/\.*`,
-          `${__dirname}/.cache/`,
-        ]
+        ignore: [`**/\.*`, `${__dirname}/.cache/`],
       },
     },
     {
       resolve: `gatsby-transformer-remark`,
       options: {
-        plugins: [{
-          resolve: `gatsby-remark-prismjs`,
-          options: {
-            classPrefix: "language-",
-            inlineCodeMarker: null,
-            aliases: {},
-            showLineNumbers: true,
-            noInlineHighlight: false,
+        plugins: [
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: "language-",
+              inlineCodeMarker: null,
+              aliases: {},
+              showLineNumbers: true,
+              noInlineHighlight: false,
+            },
           },
-        }],
-      }
+        ],
+      },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-typescript`,
@@ -40,15 +39,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `浅色圆锥曲线爱好者`,
+        short_name: `blog`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#1d1d1d`,
+        theme_color: `#333333`,
         display: `minimal-ui`,
-        icon: `src/imgs/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `static/eon.jpg`, // This path is relative to the root of the site.
       },
     },
-     `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
