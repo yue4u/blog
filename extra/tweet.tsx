565a44a7849b684a8ec209b75293ff7ef9bb9919
rainy-me <github@rainy.me>
2019-12-07 20:03:56 +0900

    add tweet support

---
+++ extra/tweet.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from "react"
+type TweetProps = {
+  url: string
+}
+export default function Tweet({ url }: TweetProps) {
+  useEffect(() => {
+    setTimeout(() => {
+      if (window.twttr) return
+      const [d, s, id] = [document, "script", "twitter-wjs"]
+
+      const t = window.twttr || {}
+      if (d.getElementById(id)) return t
+
+      const js = d.createElement(s)
+      js.id = id
+      js.src = "https://platform.twitter.com/widgets.js"
+
+      const fjs = d.getElementsByTagName(s)[0]
+      fjs.parentNode.insertBefore(js, fjs)
+      t._e = []
+      t.ready = f => t._e.push(f)
+
+      window.twttr = t
+    }, 2000)
+  }, [])
+  return (
+    <blockquote
+      className="twitter-tweet"
+      data-theme="dark"
+      dnt="true"
+      align="center"
+      style={{ minHeight: "200px" }}
+    >
+      <a href={url}>{url}</a>
+    </blockquote>
+  )
+}
--- gatsby-browser.js
+++ gatsby-browser.js
@@ -12,3 +12,26 @@ exports.shouldUpdateScroll = ({
     }
     return false
 }
+
+
+const embedClasses = [
+    `.twitter-tweet`,
+    `.twitter-timeline`,
+    `.twitter-follow-button`,
+    `.twitter-share-button`,
+].join(`,`)
+
+exports.onRouteUpdate = () => {
+    // If there's an embedded element, lazy-load the twitter script (if it hasn't
+    // already been loaded), and then run the twitter load function.
+    if (document.querySelector(embedClasses) !== null) {
+
+        if (
+            typeof twttr !== `undefined` &&
+            window.twttr.widgets &&
+            typeof window.twttr.widgets.load === `function`
+        ) {
+            window.twttr.widgets.load()
+        }
+    }
+}
