07dae9d40fdec5c00e11964e72b7d96e2a080e44
rainy-me <github@rainy.me>
2021-02-17 06:09:32 +0900

    goodbye disqus hello utteranc.es

--- src/components/comments.tsx
+++ src/components/comments.tsx
@@ -1,24 +1,35 @@
-import React from "react"
+import React, { useEffect, useRef } from "react"
 import styled from "styled-components"
-import { DiscussionEmbed } from "disqus-react"
 
-type DisqusConfig = DiscussionEmbed["props"]["config"]
+export function Comments() {
+  const commentBox = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (!commentBox.current || commentBox.current?.children.length) {
+      return
+    }
+    let scriptEl = document.createElement("script")
+    scriptEl.setAttribute("src", "https://utteranc.es/client.js")
+    scriptEl.setAttribute("crossorigin", "anonymous")
+    scriptEl.setAttribute("async", "true")
+    scriptEl.setAttribute("repo", "rainy-me/blog")
+    scriptEl.setAttribute("issue-term", "pathname")
+    scriptEl.setAttribute("theme", "github-dark")
+    commentBox.current.appendChild(scriptEl)
+  }, [])
+
+  return (
+    <CommentBlock id="comments">
+      <div ref={commentBox} />
+    </CommentBlock>
+  )
+}
 
 const CommentBlock = styled.div`
   margin: 0 auto;
   width: 80%;
-  padding-bottom: 8rem;
+  padding-bottom: 2rem;
   @media screen and (max-width: 700px) {
     width: 90%;
   }
 `
-export function Comments({ url, identifier, title }: DisqusConfig) {
-  const disqusShortname = "yue-blog"
-  const disqusConfig = { url, identifier, title }
-  // return (
-  //   <CommentBlock>
-  //     <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
-  //   </CommentBlock>
-  // )
-  return <></>
-}
